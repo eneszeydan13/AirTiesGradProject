fc6520abb35d3259b6cfd8b17d21c9bd9a1bb4e0
eneszeydan13 <enes.zeydan13@gmail.com>
2022-05-18 11:19:42 +0300

    Initial commit

---
+++ app/src/main/java/com/eneszeydan/airtiesgradproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eneszeydan.airtiesgradproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
