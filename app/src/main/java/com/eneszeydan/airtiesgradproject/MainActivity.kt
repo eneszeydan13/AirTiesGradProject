4d05468efddaeec3467ba093bf0293fb94050862
eneszeydan13 <enes.zeydan13@gmail.com>
2022-05-21 10:38:06 +0300

    Navigation components added

--- app/src/main/java/com/eneszeydan/airtiesgradproject/MainActivity.kt
+++ app/src/main/java/com/eneszeydan/airtiesgradproject/MainActivity.kt
@@ -2,10 +2,24 @@ package com.eneszeydan.airtiesgradproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.eneszeydan.airtiesgradproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+        //This is to get rid of the shadow that was showing up in the bottom navigation view for some reason
+        binding.bottomNavigationView.background = null
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navHostFragment.navController)
+
+
     }
 }
