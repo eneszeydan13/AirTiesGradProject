0194e1a02efa5a427cf3aafe7b4f57d61b79768c
eneszeydan13 <enes.zeydan13@gmail.com>
2022-06-01 22:27:15 +0300

    SearchView added to SearchFragment

--- app/src/main/java/com/eneszeydan/airtiesgradproject/viewmodels/SearchViewModel.kt
+++ app/src/main/java/com/eneszeydan/airtiesgradproject/viewmodels/SearchViewModel.kt
@@ -1,4 +1,22 @@
 package com.eneszeydan.airtiesgradproject.viewmodels
 
-class SearchViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.eneszeydan.airtiesgradproject.entity.Food
+import com.eneszeydan.airtiesgradproject.repo.FoodsDaoRepository
+
+class SearchViewModel : ViewModel() {
+    var foods = MutableLiveData<List<Food>>()
+    val frepo = FoodsDaoRepository()
+
+
+    init {
+        loadFoods()
+        foods = frepo.getFoods()
+    }
+
+
+    fun loadFoods(){
+        frepo.getAllFoods()
+    }
 }
